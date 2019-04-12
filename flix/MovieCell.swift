e0a9d639196f797d24766a4ad294fd2d9f7a7ffb
Darrel Muonekwu <dmuonekw@uci.edu>
2019-04-12 12:00:34 -0700

    Initial Commit

---
+++ flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Darrel Muonekwu on 4/7/19.
+//  Copyright © 2019 Darrel Muonekwu. All rights reserved.
+//
+
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var summeryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
